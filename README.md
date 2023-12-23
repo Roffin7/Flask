46c0a9ce22712ef4982a7ee15f05c5c40dccd8c2
Roffin7 <roffinjason@gmail.com>
2023-12-23 12:55:30 +0530

    Remodelling all files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lab-flask
+# Flask
 
 <!-- ![image](https://user-images.githubusercontent.com/115451707/196919992-edcfea8b-e3f6-4f35-9398-43be66b5622d.png) -->
 
@@ -12,5 +12,5 @@ python app.py
 
 To access your flask application open new tab in and paste the url:
 ```
-https://{your_url}.pwskills.app:5000/
+http://192.168.43.233:5000
 ```
--- app.py
+++ app.py
@@ -1,57 +1,53 @@
-from flask import Flask,request ,render_template , jsonify
+from flask import Flask,request,render_template,jsonify
 
-app = Flask(__name__)
-
+app=Flask(__name__)
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-
 @app.route('/math',methods=['POST'])
-def math_ops():
-    if(request.method == 'POST'):
-        ops = request.form['operation']
-        num1 = int(request.form['num1'])
-        num2 = int(request.form['num2'])
-        if ops == 'add':
-            r = num1+num2
-            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'subtract':
-            r = num1-num2
-            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'multiply':
-            r = num1*num2
-            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'divide':
-            r = num1/num2
-            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-            
-        return render_template('results.html' , result = result)
-
-
-
-
-@app.route('/postman_action',methods=['POST'])
 def math_ops1():
-    if(request.method == 'POST'):
-        ops = request.json['operation']
-        num1 = int(request.json['num1'])
-        num2 = int(request.json['num2'])
+    if request.method == 'POST':
+        ops=request.form['operation']
+        num1=int(request.form['num1'])
+        num2=int(request.form['num2'])
+        
         if ops == 'add':
-            r = num1+num2
-            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            result=f"The sum of {num1} and {num2} : {num1+num2}"
         if ops == 'subtract':
-            r = num1-num2
-            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            result=f"The subtract of {num1} and {num2} : {num1-num2}"        
         if ops == 'multiply':
-            r = num1*num2
-            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            result=f"The multiply of {num1} and {num2} : {num1*num2}"
         if ops == 'divide':
-            r = num1/num2
-            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-            
-        return jsonify(result)
+            try:
+                result=f"The divide of {num1} and {num2} : {num1/num2}"
+            except:
+                result=f"Zero Division error,if num2=0"
+    return render_template('results.html',result=result)
 
-if __name__=="__main__":
+# Check Using Postman
+@app.route('/json',methods=['POST'])
+def math_ops():
+    if request.method == 'POST':
+        ops=request.json['operation']
+        num1=int(request.json['num1'])
+        num2=int(request.json['num2'])
+        
+        if ops == 'add':
+            result=f"The sum of {num1} and {num2} : {num1+num2}"
+        if ops == 'subtract':
+            result=f"The subtract of {num1} and {num2} : {num1-num2}"        
+        if ops == 'multiply':
+            result=f"The multiply of {num1} and {num2} : {num1*num2}"
+        if ops == 'divide':
+            try:
+                result=f"The divide of {num1} and {num2} : {num1/num2}"
+            except:
+                result=f'Zero Division error,if num2=0'
+    return jsonify(result)
+    
+    
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
+                
